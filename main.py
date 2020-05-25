260d604107f9a0f1fdc639d18c0e857d6c52dad1
Viktoria Schaffer <vicky.schaffer@gmx.at>
2020-05-25 17:43:29 +0200

    add authentic

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import uuid
 from flask import Flask, render_template, request, redirect, url_for, make_response
 import random
 import datetime
+import hashlib
 from models import User, SecretNumberStore, db
 
 
@@ -10,6 +11,9 @@ app = Flask(__name__)
 db.create_all()
 
 COOKIE_ID_STRING = "lucky_number/secret_number_identifier"
+
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
 
 @app.route("/")
 def main():
@@ -87,13 +91,96 @@ def register():
         return render_template("register.html", **context)
     elif request.method == "POST":
         username = request.form.get("username")
-        new_user = User(name=username)
+        password = request.form.get("password")
+
+        # hash password, protect aggainst DB breaches
+        hashed_password = hash_password(password)
+        # save hashed password in the database with the new user
+        new_user = User(name=username, password=hashed_password, secret_number=10)
         db.add(new_user)
         db.commit() # abspeichern alles geaddeten elemente, in einer transaktion.
+
         return redirect(url_for('register'))
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method== "GET":
+        return render_template("login.html")
+    elif request.method=="POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
 
+        hashed_password = hash_password(password)
+        user = db.query(User).filter_by(name=username, password=hashed_password).first()
+        if user is not None:
 
+            #create new login token for the user and save it in the DB
+            user.login_token = str(uuid.uuid4())
+            db.add(user)
+            db.commit()
+
+            # set token in the browser, to identify logged in user in the future.
+            response = make_response(redirect(url_for('main')))
+            response.set_cookie('login_token', user.login_token)
+
+            return response
+        else:
+            return redirect(url_for('login'))
+
+@app.route("/logout", methods=["GET"])
+def logout():
+    # identify user in the DB based on token, if token is available
+    login_token = request.cookies.get("login_token")
+    # if no cookie is available, then redirect to login page directly
+    if login_token is None:
+        return redirect(url_for("login"))
+
+    # if cookie is available, find user with that login token, and :
+            # - remove token in DB (= overwrite with "")
+    user = db.query(User).filter_by(login_token=login_token).first()
+    if user is not None:
+        user.login_token = ""
+        db.add(user)
+        db.commit()
+
+            # - remove cookie from browser
+        response = make_response(redirect(url_for('login')))
+        response.set_cookie("login_token", expires=0)
+        return response
+
+    # redirect to login page
+    return redirect(url_for("login"))
+
+@app.route("/users/<user_id>/edit", methods=["GET", "POST"])
+def edit_user(user_id):
+    user = db.query(User).get(int(user_id))
+    if user is None:
+        return redirect(url_for('register'))
+
+    if request.method == "GET":
+        return render_template("user_edit.html", user=user)
+    elif request.method == "POST":
+        secret_number = request.form.get("secret_number")
+        login_token = request.form.get("login_token")
+
+        user.secret_number = secret_number
+        user.login_token = login_token
+
+        db.add(user)
+        db.commit()
+
+        return redirect(url_for('register'))
+
+@app.route("/users/<user_id>/delete", methods=["GET"])
+def delete_user(user_id):
+    user = db.query(User).get(int(user_id))
+    if user is None:
+        return redirect(url_for("register"))
+    if request.method == "GET":
+        response = make_response(redirect(url_for('register')))
+        db.delete(user)
+        db.commit()
+        return response
 
 @app.route("/friseursalon")
 def friseursalon():
--- models.py
+++ models.py
@@ -1,12 +1,14 @@
 import os
 from sqla_wrapper import SQLAlchemy
 
-db = SQLAlchemy(os.getenv("DATABASE_URL", "postgres://cseitffdkftjax:3cf4cd23efd56eab42a3245d8490817261b3bedff216910dc638e79f16642f43@ec2-54-246-90-10.eu-west-1.compute.amazonaws.com:5432/d3tsrlcgkeiqj0"))
+db = SQLAlchemy(os.getenv("DATABASE_URL", "sqlite:///localhost.sqlite"))
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True)
     secret_number = db.Column(db.Integer, nullable=True)
+    password = db.Column(db.String, nullable=True)
+    login_token = db.Column(db.String, nullable=True)
 
 class SecretNumberStore(db.Model):
     id = db.Column(db.Integer, primary_key=True)
